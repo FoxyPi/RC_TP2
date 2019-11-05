7829e5b757081d67a7a751b8ec8fdd48dd24ff03
dmsapereira <david.pinguim2@hotmail.com>
2019-11-05 19:12:13 +0000

    Falta os Stats

--- GetFile.java
+++ GetFile.java
@@ -12,47 +12,56 @@ import java.net.URL;
 
 public class GetFile{
 	private static final int BUF_SIZE = 512;
+	private static final String REQUEST_FORMAT = "GET %s HTTP/1.0\r\n" + "Host: %s\r\n" + "Range: bytes=%d-%d\r\n" + "User-Agent: X-RC2018\r\n\r\n";
+	private static final int BLOCK_SIZE = 1048576; //1Mbyte
 	public static void main(String[] args) throws Exception {
 		if ( args.length != 2 ) {
 			System.out.println("Usage: java HttpClientDemo url_to_access output_filename");
 			System.exit(0);
 		}
         String url = args[0];
-        String fileName = args[1];
+		String fileName = args[1];
+		int offset = 0;
+		String answerLine;
 		URL u = new URL(url);
 		// Assuming URL of the form http://server-name/path ....
 		int port = u.getPort() == -1 ? 80 : u.getPort();
 		String path = u.getPath() == "" ? "/" : u.getPath();
-        Socket sock = new Socket( u.getHost(), port );
-        OutputStream out = sock.getOutputStream();
         FileOutputStream fout = new FileOutputStream(fileName);
-		InputStream in = sock.getInputStream();
 		
-		String request = String.format(
-				"GET %s HTTP/1.0\r\n"+
-				"Host: %s\r\n"+
-				"User-Agent: X-RC2018\r\n\r\n", path, u.getHost());
-		out.write(request.getBytes());
-		
-		System.out.println("\nSent:\n\n"+request);
-		System.out.println("Got:\n");
-		
-		String answerLine = Http.readLine(in);  // first line is always present
-		System.out.println(answerLine);
-		answerLine = Http.readLine(in);
-		while ( !answerLine.equals("") ) {
+		for(;;){
+			Socket sock = new Socket( u.getHost(), port );
+			OutputStream out = sock.getOutputStream();
+			InputStream in = sock.getInputStream();
+
+			String request = String.format(REQUEST_FORMAT, path, u.getHost(),offset, offset + BLOCK_SIZE);
+			out.write(request.getBytes());
+			
+			System.out.println("\nSent:\n\n"+request);
+			System.out.println("Got:\n");
+			
+			if (Http.parseHttpReply(answerLine = Http.readLine(in))[1].equals("416"))
+				break;
+				
+			// first line is always present
 			System.out.println(answerLine);
-			answerLine = Http.readLine(in);
+			while ( !answerLine.equals("") ) {
+				System.out.println(answerLine);
+				answerLine = Http.readLine(in);
+			}
+			
+			int n;
+			byte[] buffer = new byte[BUF_SIZE];
+			while( (n = in.read(buffer) ) > 0 ) {
+				offset += n;
+				fout.write(buffer, 0, n);
+			}
+
+			sock.close();
+
+
 		}
-		
-		System.out.println("\nPayload:\n See file teste.HTML");
-		int n;
-		byte[] buffer = new byte[BUF_SIZE];
-		while( (n = in.read(buffer) ) > 0 ) {
-            fout.write(buffer, 0, n);
-        }
         fout.close();
-		sock.close();
 	}
 
 }
